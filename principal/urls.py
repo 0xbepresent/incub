990b1aab80679f148715ae9094e4923b0f52e604
Misael <arensiatik@hotmail.com>
2012-12-27 22:33:10 -0600

    Heroku configuration

--- principal/urls.py
+++
@@ -1,6 +0,0 @@
-from django.conf.urls.defaults import patterns, url
-
-urlpatterns = patterns('principal.views',
-    url(r'^$', 'home', name='home'),
-    url(r'^xhr_terminal/$', "xhr_terminal", name="xhr_terminal"),
-    )
